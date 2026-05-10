610d08cfc2901902500c412e33e77d17cef293e1
leono0999 <carlos.cueva.ec@gmail.com>
2026-05-10 16:08:08 -0500

    new add

---
+++ new_excercise.py
@@ -0,0 +1,7 @@
+import random
+f_name = input('Type the file name: ')
+f = open(f_name) # "r" omitted as it's the default
+f_content = f.read()
+f_content_list = f_content.split("\n")
+f.close()
+print(random.choice(f_content_list))
